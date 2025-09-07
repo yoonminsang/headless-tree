e6053bbacb86b4f98b365e854b6ebbcbb34513ac
yoonminsang <apdlyooapdl@gmail.com>
2025-09-08 00:29:23 +0900

    feat: HeadlessTree에서 사용하는 타입 지정

---
+++ src/HeadlessTree/types.ts
@@ -0,0 +1,59 @@
+import type { ReactNode } from 'react';
+
+export type TreeItemId = number | string;
+
+export interface BasicTreeItem<CustomData = unknown> {
+  id: TreeItemId;
+  /** List of child IDs for the tree item */
+  children: TreeItemId[];
+  isOpened?: boolean;
+  customData: CustomData;
+}
+
+export interface TreeData<CustomData> {
+  rootIds: TreeItemId[];
+  items: Record<TreeItemId, CustomData>;
+}
+
+export interface RenderItemParams<CustomData> {
+  item: CustomData;
+  depth: number;
+  /** Parent ID of the current tree item, null if it's a root item */
+  parentId: TreeItemId | null;
+  // NOTE: isFirstTreeInSameDepth is excluded as it's not considered necessary
+  isLastTreeInSameDepth: boolean;
+  /**
+   * @description Hash table tracking whether vertical connecting lines should be drawn at each depth (whether all items are expanded at the current tree item's depth)
+   *
+   * @example Needed when drawing vertical connecting lines during tree rendering.
+   *
+   * For example:
+   * ├── Node 1
+   * │   ├── Child 1
+   * │   └── Child 2
+   * └── Node 2
+   *     └── Child 3
+   *
+   * To decide whether to draw the │ line (leftmost),
+   * you need to know if there are "siblings left to render" at each depth.
+   */
+  completeDepthHashTable: Record<number, true>;
+  open: VoidFunction;
+  close: VoidFunction;
+  toggleOpenState: VoidFunction;
+}
+
+export interface TreeOptions {
+  /**
+   * If true, synchronizes the tree with initialTree whenever initialTree changes.
+   * Useful when tree data needs to be modified from outside the tree component.
+   * However, memoization handling such as useState, useMemo, select, etc. is required for use.
+   */
+  syncWithInitialTree?: boolean;
+}
+
+export interface TreeProps<CustomData extends BasicTreeItem> {
+  initialTree: TreeData<CustomData>;
+  options?: TreeOptions;
+  renderItem: (item: RenderItemParams<CustomData>) => ReactNode;
+}
