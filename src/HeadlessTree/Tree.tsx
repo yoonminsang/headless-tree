bd54bc7fec256666fbc4579a18e341430980ac97
yoonminsang <apdlyooapdl@gmail.com>
2025-09-09 00:39:08 +0900

    fix: forwardRef에서 발생하는 제네릭 문제 해소

--- src/HeadlessTree/Tree.tsx
+++ src/HeadlessTree/Tree.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, useImperativeHandle, useMemo } from 'react';
+import { forwardRef, useImperativeHandle, useMemo, type ReactElement, type Ref } from 'react';
 import { flattenTree } from './flattenTree';
 import type { BasicTreeItem, TreeProps } from './types';
 import { useTreeState } from './useTreeState';
@@ -6,39 +6,44 @@ import { useTreeState } from './useTreeState';
 export type TreeRef<T extends BasicTreeItem> = ReturnType<typeof useTreeState<T>>;
 
 /** @description Headless Tree Component */
-export const Tree = forwardRef<TreeRef<BasicTreeItem>, TreeProps<BasicTreeItem>>(
-  <T extends BasicTreeItem>({ initialTree, options, renderItem }: TreeProps<T>, ref: React.Ref<TreeRef<T>>) => {
-    const { tree, open, close, toggleOpen, openAll, closeAll } = useTreeState({ initialTree, options });
-    const flattenedTree = useMemo(() => flattenTree(tree), [tree]);
+function TreeComponent<T extends BasicTreeItem>(
+  { initialTree, options, renderItem }: TreeProps<T>,
+  ref: Ref<TreeRef<T>>
+) {
+  const { tree, open, close, toggleOpen, openAll, closeAll } = useTreeState({ initialTree, options });
+  const flattenedTree = useMemo(() => flattenTree(tree), [tree]);
 
-    useImperativeHandle(
-      ref,
-      () => ({
-        tree,
-        open,
-        close,
-        toggleOpen,
-        openAll,
-        closeAll,
-      }),
-      [tree, open, close, toggleOpen, openAll, closeAll]
-    );
+  useImperativeHandle(
+    ref,
+    () => ({
+      tree,
+      open,
+      close,
+      toggleOpen,
+      openAll,
+      closeAll,
+    }),
+    [tree, open, close, toggleOpen, openAll, closeAll]
+  );
 
-    return (
-      <>
-        {flattenedTree.map((item) =>
-          renderItem({
-            open: () => open(item.item.id),
-            close: () => close(item.item.id),
-            depth: item.depth,
-            isLastTreeInSameDepth: item.isLastTreeInSameDepth,
-            completeDepthHashTable: item.completeDepthHashTable,
-            item: item.item,
-            parentId: item.parentId,
-            toggleOpenState: () => toggleOpen(item.item.id),
-          })
-        )}
-      </>
-    );
-  }
-);
+  return (
+    <>
+      {flattenedTree.map((item) =>
+        renderItem({
+          open: () => open(item.item.id),
+          close: () => close(item.item.id),
+          depth: item.depth,
+          isLastTreeInSameDepth: item.isLastTreeInSameDepth,
+          completeDepthHashTable: item.completeDepthHashTable,
+          item: item.item,
+          parentId: item.parentId,
+          toggleOpenState: () => toggleOpen(item.item.id),
+        })
+      )}
+    </>
+  );
+}
+
+export const Tree = forwardRef(TreeComponent) as <T extends BasicTreeItem>(
+  props: TreeProps<T> & { ref?: React.Ref<TreeRef<T>> }
+) => ReactElement;
